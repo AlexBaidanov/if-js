070da84307254d5b0a3351a32cdf77b5b2b2d199
AlexBaidanov <alexbaidanov95@gmail.com>
2023-05-26 19:38:13 +0300

    write test variable and arrays

--- main.js
+++ main.js
@@ -9,5 +9,42 @@ console.log(user);
 
 console.log('-----');
 
+let test;
+test = 1;
+test++;
+test += 1;
+console.log(test);
+test--;
+console.log(test);
+test = true;
+console.log(test);
 
+console.log('-----');
 
+const mass = [2, 3, 5, 8];
+
+let result = 1;
+for (let i = 0; i < mass.length; i++) {
+  result = result * mass[i];
+}
+console.log(result);
+
+console.log('-----');
+
+const arr = [2, 5, 8, 15, 0, 6, 20, 3];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 5 && arr[i] < 10) {
+    console.log(arr[i]);
+  }
+}
+
+console.log('-----');
+
+const massarr = [2, 5, 8, 15, 0, 6, 20, 3];
+
+for (let i = 0; i < massarr.length; i++) {
+  if (massarr[i] % 2 === 0) {
+    console.log(massarr[i]);
+  }
+}
