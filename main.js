fc1271121e7e350c471db360cf39ea6146453007
AlexBaidanov <alexbaidanov95@gmail.com>
2023-05-30 13:13:30 +0300

    write min & max functions

--- main.js
+++ main.js
@@ -1,52 +1,19 @@
-let user = 'John Doe';
-console.log(user);
+const min = function (a,b) {
 
-const student = 'Alex';
-console.log(student);
-
-user = student;
-console.log(user);
-
-console.log('-----');
-
-let test;
-test = 1;
-console.log(test);
-test++;
-console.log(test);
-test += '1';
-console.log(test);
-test--;
-console.log(test);
-test = true;
-console.log(test);
-
-console.log('-----');
-
-const mass = [2, 3, 5, 8];
-
-let result = 1;
-for (let i = 0; i < mass.length; i++) {
-  result = result * mass[i];
+if (a <= b) {
+  return a;
 }
-console.log(result);
+return b;
+};
+console.log(min(5,25));
 
 console.log('-----');
 
-const arr = [2, 5, 8, 15, 0, 6, 20, 3];
+const max = function (a,b) {
 
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] > 5 && arr[i] < 10) {
-    console.log(arr[i]);
+  if (a >= b) {
+    return a;
   }
-}
-
-console.log('-----');
-
-const massarr = [2, 5, 8, 15, 0, 6, 20, 3];
-
-for (let i = 0; i < massarr.length; i++) {
-  if (massarr[i] % 2 === 0) {
-    console.log(massarr[i]);
-  }
-}
+  return b;
+};
+console.log(max(5,25));
