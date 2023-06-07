1ad6c8634b4d04355e5f34746c0b0203c2e42e49
AlexBaidanov <alexbaidanov95@gmail.com>
2023-06-07 12:15:39 +0300

    create sum function

--- main.js
+++ main.js
@@ -1,46 +1,12 @@
-const pali = 'шалаш';
-console.log(pali);
+(() => {
+  function sum() {
+    const numA = Math.random() * 10 + 1;
+    const numB = Math.random() * 10 + 1;
 
-const palindrome = function (pali) {
-  const check = pali.split('').reverse().join('');
-  console.log(check);
-
-  if (pali === check) {
-    return 'true';
+    return function () {
+      return numA + numB;
+    };
   }
-  return 'false';
-};
-console.log(palindrome(pali));
 
-console.log('-----');
-
-const min = function (a, b) {
-  if (a <= b) {
-    return a;
-  }
-  return b;
-};
-console.log(min(5, 25));
-
-console.log('-----');
-
-const max = function (a, b) {
-  if (a >= b) {
-    return a;
-  }
-  return b;
-};
-console.log(max(5, 25));
-
-console.log('-----');
-
-const arr = [10, 25, 30, 45, 50, 65, 70, 85, 90, 1];
-console.log(arr);
-const change = function (numb) {
-  const replase = `${numb}`;
-  if (replase.includes('0')) {
-    return `${numb}`.replaceAll('0', 'zero');
-  }
-  return numb;
-};
-console.log(arr.map(change).join(', '));
+  console.log(sum()());
+})();
