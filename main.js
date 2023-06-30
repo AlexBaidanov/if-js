db9e96fe65cc81aeeebf782137448c12bd07ab1e
AlexBaidanov <alexbaidanov95@gmail.com>
2023-06-30 21:30:01 +0300

    add search function

--- main.js
+++ main.js
@@ -63,3 +63,65 @@ const newDate = date.replace(/(\d{4})\D(\d{1,2})\D(\d{1,2})/g, replacer);
 console.log(newDate);
 
 console.log('..............');
+
+const data = [
+  {
+    country: 'Russia',
+    city: 'Saint Petersburg',
+    hotel: 'Hotel Leopold',
+  },
+  {
+    country: 'Spain',
+    city: 'Santa Cruz de Tenerife',
+    hotel: 'Apartment Sunshine',
+  },
+  {
+    country: 'Slowakia',
+    city: 'Vysokie Tatry',
+    hotel: 'Villa Kunerad',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hostel Friendship',
+  },
+  {
+    country: 'Indonesia',
+    city: 'Bali',
+    hotel: 'Ubud Bali Resort&SPA',
+  },
+  {
+    country: 'Netherlands',
+    city: 'Rotterdam',
+    hotel: 'King Kong Hostel',
+  },
+  {
+    country: 'Marocco',
+    city: 'Ourika',
+    hotel: 'Rokoko Hotel',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hotel Rehberge Berlin Mitte',
+  },
+];
+
+const search = (data, search) => {
+  const results = [];
+
+  const regex = new RegExp(search, 'i');
+
+  for (const obj of data) {
+    const { country, city, hotel } = obj;
+
+    if (regex.test(country) || regex.test(city) || regex.test(hotel)) {
+      results.push(`${country}, ${city}, ${hotel}`);
+    }
+  }
+
+  return results;
+};
+
+const results = search(data, 'germany');
+console.log(results);
