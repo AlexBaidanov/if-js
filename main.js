31c0b967a71a1ca1d489993c08e4ace34c44fe60
AlexBaidanov <alexbaidanov95@gmail.com>
2023-07-21 17:40:33 +0300

    add class Students

--- main.js
+++ main.js
@@ -1,25 +1,73 @@
 class User {
-  constructor({ firstName, lastName }) {
+  constructor(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
   }
-  fullName() {
+  get fullName() {
     return `${this.firstName} ${this.lastName}`;
   }
 }
 
-console.log(User);
-
 class Student extends User {
-  constructor({ admissionYear, courseName, ...props }) {
-    super(...props);
+  constructor(firstName, lastName, admissionYear, courseName) {
+    super(firstName, lastName);
     this.admissionYear = admissionYear;
     this.courseName = courseName;
   }
-  course() {
-    const currentYear = new Date().getFullYear();
+  get course() {
+    const currentYear = 2023;
     return currentYear - this.admissionYear;
   }
 }
 
-console.log(Student);
+class Students {
+  constructor(students) {
+    this.students = students.map(
+      (student) =>
+        new Student(
+          student.firstName,
+          student.lastName,
+          student.admissionYear,
+          student.courseName,
+        ),
+    );
+  }
+  getInfo() {
+    return this.students
+      .sort((a, b) => a.course - b.course)
+      .map(
+        (student) =>
+          `${student.fullName} - ${student.courseName}, ${student.course} курс`,
+      );
+  }
+}
+
+const studentsData = [
+  {
+    firstName: 'Василий',
+    lastName: 'Петров',
+    admissionYear: 2019,
+    courseName: 'Java',
+  },
+  {
+    firstName: 'Иван',
+    lastName: 'Иванов',
+    admissionYear: 2018,
+    courseName: 'JavaScript',
+  },
+  {
+    firstName: 'Александр',
+    lastName: 'Федоров',
+    admissionYear: 2017,
+    courseName: 'Python',
+  },
+  {
+    firstName: 'Николай',
+    lastName: 'Петров',
+    admissionYear: 2019,
+    courseName: 'Android',
+  },
+];
+
+const students = new Students(studentsData);
+console.log(students.getInfo());
