6f9b4447f0086547d266d10f4ad68604e53bd518
AlexBaidanov <alexbaidanov95@gmail.com>
2023-07-29 13:11:11 +0300

    add Symbol.iterator, that changes color of a paragraph

--- main.js
+++ main.js
@@ -1,73 +1,32 @@
-class User {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-}
+const text1 = document.getElementById('text1');
+const text2 = document.getElementById('text2');
+const text3 = document.getElementById('text3');
 
-class Student extends User {
-  constructor(firstName, lastName, admissionYear, courseName) {
-    super(firstName, lastName);
-    this.admissionYear = admissionYear;
-    this.courseName = courseName;
-  }
-  get course() {
-    const currentYear = 2023;
-    return currentYear - this.admissionYear;
-  }
-}
+const colors = {
+  data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
+  counters: {},
+  [Symbol.iterator]() {
+    return this;
+  },
+  next(id) {
+    if (this.counters[id] === undefined) {
+      this.counters[id] = 0;
+    }
+    const value = this.data[this.counters[id]++];
+    if (this.counters[id] >= this.data.length) {
+      this.counters[id] = 0;
+    }
+    return {
+      done: false,
+      value,
+    };
+  },
+};
 
-class Students {
-  constructor(students) {
-    this.students = students.map(
-      (student) =>
-        new Student(
-          student.firstName,
-          student.lastName,
-          student.admissionYear,
-          student.courseName,
-        ),
-    );
-  }
-  getInfo() {
-    return this.students
-      .sort((a, b) => a.course - b.course)
-      .map(
-        (student) =>
-          `${student.fullName} - ${student.courseName}, ${student.course} курс`,
-      );
-  }
-}
+const changeStyle = (id) => (event) => {
+  event.target.style.color = colors.next(id).value;
+};
 
-const studentsData = [
-  {
-    firstName: 'Василий',
-    lastName: 'Петров',
-    admissionYear: 2019,
-    courseName: 'Java',
-  },
-  {
-    firstName: 'Иван',
-    lastName: 'Иванов',
-    admissionYear: 2018,
-    courseName: 'JavaScript',
-  },
-  {
-    firstName: 'Александр',
-    lastName: 'Федоров',
-    admissionYear: 2017,
-    courseName: 'Python',
-  },
-  {
-    firstName: 'Николай',
-    lastName: 'Петров',
-    admissionYear: 2019,
-    courseName: 'Android',
-  },
-];
-
-const students = new Students(studentsData);
-console.log(students.getInfo());
+text1.addEventListener('click', changeStyle('text1'));
+text2.addEventListener('click', changeStyle('text2'));
+text3.addEventListener('click', changeStyle('text3'));
