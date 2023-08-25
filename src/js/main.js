c355e9e2b6fa17a47da5634e96b8e464d3a557b8
AlexBaidanov <alexbaidanov95@gmail.com>
2023-08-25 13:30:50 +0300

    add homes block content using fetch request

--- src/js/main.js
+++ src/js/main.js
@@ -1,53 +1,24 @@
-import data from './homesData.js';
-
-const homesSection = document.createElement('section');
-homesSection.setAttribute('class', 'homes');
-document.body.appendChild(homesSection);
-
-const homesDiv = document.createElement('div');
-homesDiv.setAttribute('class', 'container');
-homesSection.appendChild(homesDiv);
-
-const homesTitle = document.createElement('h2');
-homesTitle.textContent = 'Homes guests loves';
-homesTitle.classList.add('homes__title', 'text__center', 'title');
-homesDiv.appendChild(homesTitle);
-
-const variantsDiv = document.createElement('div');
-variantsDiv.classList.add('homes__variants', 'description');
-homesDiv.appendChild(variantsDiv);
-
-const homeSection = document.querySelector('.homes__variants');
-
-const homeSectionContent = data
-  .slice(0, 4)
-  .map((element) => {
-    return `
-    <figure class="homes__item _mobile">
-      <img class="homes__image" src="${element.imageUrl}" alt="${element.name}">
-      <figcaption class="homes__description">
-        ${element.name}
-      </figcaption>
-      <figcaption class="homes__place">
-        ${element.city}, ${element.country}
-      </figcaption>
-    </figure>
-  `;
-  })
-  .join('');
-
-homeSection.innerHTML += homeSectionContent;
-
-const homesButtonEllipse = document.createElement('button');
-homesButtonEllipse.classList.add('homes__arrow--ellipse', 'button');
-homesDiv.appendChild(homesButtonEllipse);
-
-const homesButtonPike = document.createElement('div');
-homesButtonPike.classList.add('homes__arrow--pike');
-homesButtonEllipse.appendChild(homesButtonPike);
-
-const mainSection = document.querySelector('.main__content');
-
-const destinationSection = document.querySelector('.destination');
-
-mainSection.insertBefore(homesSection, destinationSection);
+function displayHotels() {
+  fetch('https://if-student-api.onrender.com/api/hotels/popular')
+    .then((response) => response.json())
+    .then((data) => {
+      const hotels = data.slice(0, 4);
+      let content = '';
+      hotels.forEach((hotel) => {
+        content += `
+          <figure class="homes__item _mobile">
+            <img
+            class="homes__image"
+            src="${hotel.imageUrl}" alt="${hotel.name}"
+            />
+            <figcaption class="homes__description">${hotel.name}</figcaption>
+            <figcaption class="homes__place">
+                ${hotel.city}, ${hotel.country}
+            </figcaption>
+          </figure>
+    `;
+      });
+      document.querySelector('.homes__variants').innerHTML = content;
+    });
+}
+displayHotels();
